6326212543fa6c8bb8f10e833972bf9b929b5311
Kazymbetov <k.aknazar@mail.ru>
2025-07-05 18:38:12 +0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,14 @@
 pluginManagement {
-    repositories {
-        google()          // ← именно здесь ищется AGP
-        mavenCentral()
-    }
+    repositories { google(); mavenCentral() }
     plugins {
-        id("com.android.library") version "8.4.1"        // AGP ≥ 8.3 для Gradle 8.7
-        id("org.jetbrains.kotlin.android") version "1.9.24"
+        id("com.android.library")           version "8.4.1"
+        id("org.jetbrains.kotlin.android")  version "1.9.24"
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+    repositories { google(); mavenCentral() }
 }
 
 rootProject.name = "sigma-device-sdk"
