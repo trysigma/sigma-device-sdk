4620551aa9d27edcfb1e833e19bbb025e9bb542b
Kazymbetov <k.aknazar@mail.ru>
2025-07-05 18:39:07 +0500

    Update build.gradle.kts

--- sigma-sdk/build.gradle.kts
+++ sigma-sdk/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 }
 
 android {
-    namespace = "com.sigma.sdk"
-    compileSdk = 34
+    namespace   = "com.sigma.sdk"
+    compileSdk  = 34
 
     defaultConfig {
         minSdk = 21
@@ -17,15 +17,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
-    kotlin {
-        jvmToolchain(17)
-    }
+    kotlin { jvmToolchain(17) }
 
     publishing {
-        singleVariant("release") {
-            withSourcesJar()
-        }
+        singleVariant("release") { withSourcesJar() }
     }
 }
 
@@ -42,8 +37,8 @@ afterEvaluate {
                 name = "GitHub"
                 url  = uri("https://maven.pkg.github.com/trysigma/sigma-device-sdk")
                 credentials {
-                    username = System.getenv("GITHUB_ACTOR")
-                    password = System.getenv("GITHUB_TOKEN")
+                    username = findProperty("gpr.user")!!.toString()
+                    password = findProperty("gpr.key")!!.toString()
                 }
             }
         }
