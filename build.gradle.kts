56aa871734064dc8bea2ecbc4c2bb62af88467c2
Kazymbetov <k.aknazar@mail.ru>
2025-07-06 19:36:35 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.android.library")       apply false   // версии уже заданы выше
+    id("com.android.library")      apply false
     id("org.jetbrains.kotlin.android") apply false
 }
