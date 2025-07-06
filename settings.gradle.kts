7af92123edbf28e2cb73c3998768e6dd1ec8a348
Kazymbetov <k.aknazar@mail.ru>
2025-07-06 18:01:30 +0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,6 @@ pluginManagement {
     plugins {
         id("com.android.library") version "8.4.1"
         id("org.jetbrains.kotlin.android") version "1.9.24"
-        id("maven-publish")          version "1.0"
     }
 }
 
