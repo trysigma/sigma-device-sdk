eff71fe3f34542cdcbd40994382f1c61101add28
Kazymbetov <k.aknazar@mail.ru>
2025-07-06 20:52:33 +0500

    Update build.gradle.kts

--- sigma-sdk/build.gradle.kts
+++ sigma-sdk/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 }
 
 android {
-    namespace   = "com.sigma.sdk"
-    compileSdk  = 34
+    namespace = "com.sigma.sdk"
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 21
@@ -17,10 +17,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlin { jvmToolchain(17) }
+
+    kotlin {
+        jvmToolchain(17)
+    }
 
     publishing {
-        singleVariant("release") { withSourcesJar() }
+        singleVariant("release") {
+            withSourcesJar()
+        }
     }
 }
 
@@ -28,14 +33,15 @@ afterEvaluate {
     publishing {
         publications.create<MavenPublication>("release") {
             from(components["release"])
-            groupId    = "com.sigma"
+            groupId = "com.sigma"
             artifactId = "sdk"
-            version    = "1.0.0"
+            version = "1.0.0"
         }
+
         repositories {
             maven {
                 name = "GitHub"
-                url  = uri("https://maven.pkg.github.com/trysigma/sigma-device-sdk")
+                url = uri("https://maven.pkg.github.com/trysigma/sigma-device-sdk")
                 credentials {
                     username = System.getenv("GPR_USER") ?: error("Missing GPR_USER")
                     password = System.getenv("GH_PACKAGES_PAT_2") ?: error("Missing GH_PACKAGES_PAT_2")
