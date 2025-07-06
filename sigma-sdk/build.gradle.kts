b0f63b26f2180c5c5e6b52f66328081cf001399a
Kazymbetov <k.aknazar@mail.ru>
2025-07-06 23:04:54 +0500

    Update build.gradle.kts

--- sigma-sdk/build.gradle.kts
+++ sigma-sdk/build.gradle.kts
@@ -35,7 +35,7 @@ afterEvaluate {
             from(components["release"])
             groupId = "com.sigma"
             artifactId = "sdk"
-            version = "1.0.0"
+            version = "1.0.1"
         }
 
         repositories {
